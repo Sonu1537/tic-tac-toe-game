439ccbdb21c03d682b66dfe22c6964612219370d
Sonu1537 <125648716+Sonu1537@users.noreply.github.com>
2025-06-26 08:11:30 +0545

    Initial commit:Tic Tac Toe Game

---
+++ first.js
@@ -0,0 +1,18 @@
+// console.log("hello to first day of JS");
+// console.log("i love javascript");
+// name="sonu sah";
+// console.log(name);
+// age=23;
+// console.log(age);
+// let price=100.23;
+// console.log(price);
+// let myName="kaira";
+// console.log(myName);
+
+const student= {
+    name: "sonu",
+    age: 23,
+    result: "pass",
+};
+
+
